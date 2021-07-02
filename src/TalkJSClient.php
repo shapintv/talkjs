d805ff801d18341ee0b8490cedfcdd776209c0c1
Nikolaos Spyratos <nik@ankyr.dev>
2021-07-02 15:06:02 +0200

    Clean up TalkJSClient.php

--- src/TalkJSClient.php
+++ src/TalkJSClient.php
@@ -12,30 +12,23 @@ namespace CarAndClassic\TalkJS;
 use CarAndClassic\TalkJS\Api\ConversationApi;
 use CarAndClassic\TalkJS\Api\UserApi;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class TalkJSClient
 {
-    private HttpClientInterface $httpClient;
+    public UserApi $userApi;
+
+    public ConversationApi $conversationApi;
 
     public function __construct(string $appId, string $secretKey)
     {
-        $this->httpClient = HttpClient::create([
+        $httpClient = HttpClient::create([
             'base_uri' => 'https://api.talkjs.com/v1/'.$appId.'/',
             'auth_bearer' => $secretKey,
             'headers' => [
                 'Content-Type' => 'application/json',
             ],
         ]);
-    }
-
-    public function users(): UserApi
-    {
-        return new UserApi($this->httpClient);
-    }
-
-    public function conversations(): ConversationApi
-    {
-        return new ConversationApi($this->httpClient);
+        $this->userApi = new UserApi($httpClient);
+        $this->conversationApi = new ConversationApi($httpClient);
     }
 }
